47767cdaf227cdeadff56c2ca3f84b37ffd1a91c
Charanj1989 <Charanjeetkaur@flemingcollege.ca>
2024-08-05 22:24:46 -0400

    final

---
+++ Script1.py
@@ -0,0 +1,5 @@
+#Print lines from Zen of Python
+#This program is good
+#This is an amazing new feature
+print("Complex is better than complicated.")
+
---
+++ Script2.py
@@ -0,0 +1,4 @@
+#This is good programming advice
+print("Special cases aren't special enough to break the rules.")
+print("Although practicality beats purity.")
+print("Flat is better than nested.")
